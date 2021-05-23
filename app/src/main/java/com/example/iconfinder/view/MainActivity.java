3b927bfc2c563448d585371443fdab8c67b886e3
Kripa Krishnan <kripakrishnan.krish@gmail.com>
2021-05-23 20:50:38 +0530

    Pagination in categories bug fixing

--- app/src/main/java/com/example/iconfinder/view/MainActivity.java
+++ app/src/main/java/com/example/iconfinder/view/MainActivity.java
@@ -11,7 +11,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
